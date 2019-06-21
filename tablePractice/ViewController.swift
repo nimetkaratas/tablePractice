58a7503f7c35aeeb37818dc208e635cea086cae5
nimet <apple@Apples-MacBook-Pro-59.local>
2019-06-21 12:01:35 -0500

    table practice 1

---
+++ tablePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tablePractice
+//
+//  Created by Apple on 6/21/19.
+//  Copyright © 2019 Nimet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
